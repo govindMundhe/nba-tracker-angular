754820417ef4a3179b8ba1fcdd3ad38b739a2814
govindMundhe <govindmundhe452@gmail.com>
2023-02-24 22:32:34 +0530

    added dot object templates

--- src/app/team-card/team-card.component.ts
+++ src/app/team-card/team-card.component.ts
@@ -38,18 +38,18 @@ export class TeamCardComponent implements OnInit {
     let teamPoints: number[] = [];
     let oppPoints: number[] = [];
     results.forEach((result) => {
-      if (result['home_team']['id'] === id) {
-        teamPoints.push(result['home_team_score']);
-        oppPoints.push(result['visitor_team_score']);
-        if (result['home_team_score'] > result['visitor_team_score']) {
+      if (result.home_team.id === id) {
+        teamPoints.push(result.home_team_score);
+        oppPoints.push(result.visitor_team_score);
+        if (result.home_team_score > result.visitor_team_score) {
           Object.assign(result, { winStatus: 'W' });
         } else {
           Object.assign(result, { winStatus: 'L' });
         }
-      } else if (result['visitor_team']['id'] === id) {
-        teamPoints.push(result['visitor_team_score']);
-        oppPoints.push(result['home_team_score']);
-        if (result['home_team_score'] < result['visitor_team_score']) {
+      } else if (result.visitor_team.id === id) {
+        teamPoints.push(result.visitor_team_score);
+        oppPoints.push(result.home_team_score);
+        if (result.home_team_score < result.visitor_team_score) {
           Object.assign(result, { winStatus: 'W' });
         } else {
           Object.assign(result, { winStatus: 'L' });
